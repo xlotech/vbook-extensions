7e973c41fb1ba08ca44aaba730800a89fd31c6b2
xlotech <103030241+xlotech@users.noreply.github.com>
2022-10-25 18:09:55 +0700

    Update home.js

--- tangthuvien/src/home.js
+++ tangthuvien/src/home.js
@@ -1,8 +1,8 @@
 function execute() {
     return Response.success([
-        {title: "Xem nhiều", script: "gen.js", input: "https://truyen.tangthuvien.vn/tong-hop?rank=vw"},
-        {title: "Đề cử", script: "gen.js", input: "https://truyen.tangthuvien.vn/tong-hop?rank=nm"},
-        {title: "Bình luận nhiều", script: "gen.js", input: "https://truyen.tangthuvien.vn/tong-hop?rank=bl"},
-        {title: "Theo dõi nhiều", script: "gen.js", input: "https://truyen.tangthuvien.vn/tong-hop?rank=td"}
+        {title: "Xem nhiều", script: "gen.js", input: "https://truyen.tangthuvien.vn/tong-hop?rank=vw&time=m"},
+        {title: "Đề cử", script: "gen.js", input: "https://truyen.tangthuvien.vn/tong-hop?rank=nm&time=m"},
+        {title: "Bình luận nhiều", script: "gen.js", input: "https://truyen.tangthuvien.vn/tong-hop?rank=bl&time=m"},
+        {title: "Theo dõi nhiều", script: "gen.js", input: "https://truyen.tangthuvien.vn/tong-hop?rank=td&time=m"}
     ]);
 }
